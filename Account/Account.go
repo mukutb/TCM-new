cfc8bb53d8055d3e873b8102bbbd876d6f21b579
Dimple-Kanwar <dimple.kanwar@wipro.com>
2017-04-07 09:48:50 +0000

    Update Account.go

--- Account/Account.go
+++ Account/Account.go
@@ -204,9 +204,9 @@ func (t *ManageAccounts) getAccount_byName(stub shim.ChaincodeStubInterface, arg
 		fmt.Println(valueAsBytes)
 		if _tempJson.AccountName == _AccountName {
 			jsonResp = jsonResp + "\""+ val + "\":" + string(valueAsBytes[:])
-		}
-		if i < len(AccountIndex)-1 {
+			if i < len(AccountIndex)-1 {
 				jsonResp = jsonResp + ","
+			}
 		}
 	}
 	fmt.Println("len(AccountIndex) : ")
@@ -268,9 +268,9 @@ func (t *ManageAccounts) getAccount_byType(stub shim.ChaincodeStubInterface, arg
 		fmt.Println(valueAsBytes)
 		if _tempJson.AccountType == _AccountType {
 			jsonResp = jsonResp + "\""+ val + "\":" + string(valueAsBytes[:])
-		}
-		if i < len(AccountIndex)-1 {
-			jsonResp = jsonResp + ","
+			if i < len(AccountIndex)-1 {
+				jsonResp = jsonResp + ","
+			}
 		}
 	}
 	fmt.Println("len(AccountIndex) : ")
