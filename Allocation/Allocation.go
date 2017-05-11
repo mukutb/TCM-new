d8a16cbc26203b8741330011ab351e64e18edd1b
Dimple-Kanwar <dimple.kanwar@wipro.com>
2017-05-11 18:54:02 +0530

    Update Allocation.go

--- Allocation/Allocation.go
+++ Allocation/Allocation.go
@@ -276,7 +276,6 @@ func (t *ManageAllocations) LongboxAccountUpdated(stub shim.ChaincodeStubInterfa
 	}
 	_CurrentTimeObj := time.Unix(i, 0)
 	fmt.Println("_CurrentTimeObj: ", _CurrentTimeObj)
-	fmt.Println("_CurrentTimeObj hours: ", _CurrentTimeObj.Hours())
 	var newTxStatus, newAllStatus string
 
 	for _, ValueTransaction := range TransactionsDataFetched {
@@ -288,7 +287,7 @@ func (t *ManageAllocations) LongboxAccountUpdated(stub shim.ChaincodeStubInterfa
 			}
 			_MarginCallTimeObj := time.Unix(i, 0)
 			fmt.Println("_MarginCallTimeObj: ", _MarginCallTimeObj)
-			fmt.Println("_MarginCallTimeObj hours: ", _MarginCallTimeObj.Hours())
+			fmt.Println("_CurrentTimeObj.Sub(_MarginCallTimeObj).Hours(): ", _CurrentTimeObj.Sub(_MarginCallTimeObj).Hours())
 			if _CurrentTimeObj.Sub(_MarginCallTimeObj).Hours() <= 24 && _CurrentTimeObj.Sub(_MarginCallTimeObj).Hours() >= 0 {
 				// New securites are uploaded in cutoff time
 				newTxStatus = "Ready"
