d680a9509b870196243ab6fe9ea19afd322f8c85
Dimple-Kanwar <dimple.kanwar@wipro.com>
2017-05-12 17:57:16 +0530

    Update Deal.go

--- Deal/Deal.go
+++ Deal/Deal.go
@@ -1120,7 +1120,7 @@ func(t * ManageDeals) update_transaction(stub shim.ChaincodeStubInterface, args[
             `"totalValueLongBoxAccount": "` + res_Deal.TotalValueLongBoxAccount + `" , ` + 
             `"totalValueSegregatedAccount": "` + res_Deal.TotalValueSegregatedAccount + `" , ` +
             `"issueDate": "` + res_Deal.IssueDate + `" , ` + 
-            `"lastSuccessfulAllocationDate": ` + allocationDate + ` , ` +  
+            `"lastSuccessfulAllocationDate": "` + allocationDate + `" , ` +  
             `"transactions": "` + res_Deal.Transactions + `" ` + 
         `}`
         fmt.Println(deal_json)
